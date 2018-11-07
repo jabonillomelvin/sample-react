7996e9411b3fc7ca6ad783653a460ec43fe6323b
Melvin <melvin@bbo.com.ph>
2018-11-07 17:38:53 +0800

    registration page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './index.css';
-import Table from './Table';
+import Registration from './Registration';
 
 class App extends Component {
   render() {
-        const characters = [
-          {
-              'name': 'Charlie',
-              'job': 'Janitor'
-          },
-          {
-              'name': 'Mac',
-              'job': 'Bouncer'
-          },
-          {
-              'name': 'Dee',
-              'job': 'Aspring actress'
-          },
-          {
-              'name': 'Dennis',
-              'job': 'Bartender'
-          }
-      ];
-
       return (
           <div className="container">
-              <Table characterData={characters} />
+              <Registration />
           </div>
       );
   }
