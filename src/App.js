dcfaf80a017a775e3e16d30a2071bb3bef830114
Melvin <melvin@bbo.com.ph>
2018-11-08 14:45:32 +0800

    implement react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './index.css';
 import Login from './Login';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
       return (
-          <div className="container">
-              <Login />
-          </div>
+            <Router>
+                <div className="container">
+                    <Route exact path="/login" component={Login} />
+                </div>
+            </Router>
       );
   }
 }
